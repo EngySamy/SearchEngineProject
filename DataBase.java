70f42c88cef846601d1a6588cf112ff63a8cd436
AmalNafe3 <amal.nafei@gmail.com>
2017-03-16 15:52:44 +0200

    Update DataBase.java

--- DataBase.java
+++ DataBase.java
@@ -1,77 +1,104 @@
-package stopWords;
+package stemmer;
 
 import java.sql.*;
 
 public class DataBase {
     // JDBC driver name and database URL
-   static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-   static final String DB_URL = "jdbc:mysql://localhost/STUDENTS";
+   String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+   String DB_URL = "jdbc:mysql://localhost/STUDENTS";
 
    //  Database credentials
-   static final String USER = "username";
-   static final String PASS = "password";
+   String USER = "username";
+   String PASS = "password";
    
-   public static void main(String[] args) 
+   Connection conn = null;
+   Statement stmt = null;
+   
+   void connect() 
    {
-        Connection conn = null;
-        Statement stmt = null;
         try
         {
              //Register JDBC driver
-             Class.forName("com.mysql.jdbc.Driver");
+             Class.forName(JDBC_DRIVER);
 
              //Open a connection
              System.out.println("Connecting to a selected database...");
              conn = DriverManager.getConnection(DB_URL, USER, PASS);
              System.out.println("Connected database successfully...");
-
-             //Execute a query
-             System.out.println("Creating table in given database...");
-             stmt = conn.createStatement();
-
-             String sql = "CREATE TABLE REGISTRATION " +
-                          "(id INTEGER not NULL, " +
-                          " first VARCHAR(255), " + 
-                          " last VARCHAR(255), " + 
-                          " age INTEGER, " + 
-                          " PRIMARY KEY ( id ))"; 
-
-             stmt.executeUpdate(sql);
-             System.out.println("Created table in given database...");
          }
-         catch(SQLException se)
-         {
-            //Handle errors for JDBC
-            se.printStackTrace();
-         }
+         
          catch(Exception e)
          {
             //Handle errors for Class.forName
             e.printStackTrace();
          }
-         finally //block used to close resources
-         {
-              try
-              {
-                 if(stmt!=null)
-                    conn.close();
-              }
-              catch(SQLException se)
-              {
+         
+    }
+    void createTables()
+    {
+        try
+        {
+            //Execute a query
+            System.out.println("Creating tables in given database...");
+            stmt = conn.createStatement();
 
-              }
-              try
-              {
-                 if(conn!=null)
-                    conn.close();
-              }
-              catch(SQLException se)
-              {
+            String URLTable= "CREATE TABLE Websites " +
+                          "(Link_ID BIGINT not NULL, " +
+                          " Link VARCHAR(255), " + 
+                          " Document LONGTEXT, " +
+                          " PRIMARY KEY ( Link_ID ))"; 
 
-              }
-         }
+            stmt.executeUpdate(URLTable);
+            System.out.println("Created URL table in given database...");
+
+            String KeywordsTable= "CREATE TABLE Keywords " +
+                         "(Keyword_ID BIGINT not NULL, " +
+                         " Keyword VARCHAR(255), " + 
+                         " PRIMARY KEY ( Keyword_ID ))"; 
+
+            stmt.executeUpdate(KeywordsTable);
+            System.out.println("Created Keywords table in given database...");
+
+            String InfoTable= "CREATE TABLE Information " +
+                         "(FOREIGN KEY (Link_ID) REFERENCES 'Websites' (Link_ID)," +
+                         " FOREIGN KEY (Keyword_ID) REFERENCES 'Keywords' (Keyword_ID)" + 
+                         " Importance CHAR, " +
+                         " Frequency DOUBLE, " + 
+                         " PRIMARY KEY ( Link_ID , Keyword_ID ))"; 
+
+            stmt.executeUpdate(InfoTable);
+            System.out.println("Created Information table in given database...");
+        }
+        catch(SQLException se)
+        {
+            //Handle errors for JDBC
+            se.printStackTrace();
+        }
+    }
+    
+    void disconnect()
+    {
+        try
+        {
+           if(stmt!=null)
+              conn.close();
+        }
+        catch(SQLException se)
+        {
+
+        }
         
-         System.out.println("Goodbye!");
+        try
+        {
+           if(conn!=null)
+              conn.close();
+        }
+        catch(SQLException se)
+        {
+
+        }
+        
+        System.out.println("Goodbye!");
     }
    
 }
